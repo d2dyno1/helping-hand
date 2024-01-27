f2617fa29b935fc56bb08ecbbce2ab21004a1ecf
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-01-27 12:11:28 +0100

    add jquery

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
 
-wwwroot="src/dotnetBB/wwwroot"
-
-rm -f "$wwwroot/bootstrap/*"
-rm -f "$wwwroot/bootstrap-icons/*"
-
-mkdir "$wwwroot/bootstrap"
-mkdir "$wwwroot/bootstrap-icons"
+root="src/dotnetBB/wwwroot/lib"
+rm -f "$root/*"
+mkdir "$root/bootstrap"
+mkdir "$root/bootstrap-icons"
+mkdir "$root/jquery"
 
 npm i
-cp node_modules/bootstrap/dist/css/bootstrap.min.css "$wwwroot/bootstrap"
-cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js "$wwwroot/bootstrap"
-cp node_modules/bootstrap-icons/font/bootstrap-icons.min.css "$wwwroot/bootstrap-icons"
-cp -r cp node_modules/bootstrap-icons/font/fonts "$wwwroot/bootstrap-icons"
+
+cp node_modules/bootstrap/dist/css/bootstrap.min.css "$root/bootstrap"
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js "$root/bootstrap"
+
+cp node_modules/bootstrap-icons/font/bootstrap-icons.min.css "$root/bootstrap-icons"
+cp -r node_modules/bootstrap-icons/font/fonts "$root/bootstrap-icons"
+
+cp node_modules/jquery/dist/jquery.min.js "$root/jquery"
