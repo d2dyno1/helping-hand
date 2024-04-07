1ed8bb6656635899312c8a7c00f5ee5302b127bc
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-07 16:59:23 +0200

    .

--- src/PomocKolezenska/Components/Pages/Index.razor.cs
+++ src/PomocKolezenska/Components/Pages/Index.razor.cs
@@ -1,12 +1,45 @@
-﻿namespace PomocKolezenska.Components.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using PomocKolezenska.Models.Database;
+using PomocKolezenska.Models.Request;
+
+namespace PomocKolezenska.Components.Pages;
 
 public partial class Index
 {
-    private StaticModal LoginModal { get; set; } = null!;
+    [SupplyParameterFromForm]
+    private NewQuestionModel Model { get; set; } = new();
+    
+    private StaticModal NewQuestionModal { get; set; }
 
-    private Task OnLoginClick()
+    private List<Question> AllQuestions { get; set; } = new List<Question>();
+    public List<Question> UserQuestions { get; set; } = new List<Question>();
+
+    protected override async Task OnInitializedAsync()
     {
-        return LoginModal.ShowAsync();
+        AllQuestions.AddRange(ApplicationDbContext.Questions);
+        
+        var user = await UserService.GetUser(ApplicationDbContext, AuthenticationStateProvider);
+        if (user is null)
+            return;
+        UserQuestions.AddRange(user.Questions);
+    }
+
+    private async Task OnSubmitAsync()
+    {
+        var user = await UserService.GetUser(ApplicationDbContext, AuthenticationStateProvider);
+        if (user is null)
+            return;
+        
+        var question = new Question { Content = Model.Content, Title = Model.Title };
+        user.Questions.Add(question);
+        ApplicationDbContext.Update(user);
+        await ApplicationDbContext.SaveChangesAsync();
+        NavigationManager.Refresh(true);
+    }
+
+    private Task OpenNewQuestionModalAsync()
+    {
+        return NewQuestionModal.ShowAsync();
     }
 }
 
